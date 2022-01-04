b8595b2af04c0a4cd578601d3874ee757b0e7538
Patrick Wilson <pmwilson333@gmail.com>
2022-01-03 21:46:08 -0800

    router wired

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Page from './screens/home';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Make something cool here.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hello World
-          </a>
-        </span>
-      </header>
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Page />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
